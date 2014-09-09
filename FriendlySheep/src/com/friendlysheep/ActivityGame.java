181f7702b162b4f56cf5200fa5bd5ca19c763b91
Morten <mortenjansrud@gmail.ck>
2014-09-10 00:25:05 +0200

    Ball

--- FriendlySheep/src/com/friendlysheep/ActivityGame.java
+++ FriendlySheep/src/com/friendlysheep/ActivityGame.java
@@ -4,12 +4,19 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.Paint;
 import android.graphics.Point;
 import android.graphics.Rect;
 import android.os.Bundle;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.Display;
 import android.view.View;
 import android.view.View.MeasureSpec;
@@ -43,6 +50,8 @@ public class ActivityGame extends Activity{
 		activity = this;
 
 		setLayout();
+		
+		setContentView(new CustomView(activity));
 	}
 	
 	private void setLayout(){
@@ -75,9 +84,9 @@ public class ActivityGame extends Activity{
 			        	
 			        	if(position<=26) i = 0;
 			        	else if(position == 58) i = 4;
-			        	else if(position>=63) i = 3;
-			        	else if (position == 27 || 35 < position && position < 38 || 44 < position && position < 48 || 53 < position && position < 58) i = 1;
-			        	else if (position == 35 || 42 < position && position < 45 || 50 < position && position < 54 || 58 < position && position < 63) i = 2;
+			        	else if(position >= 63) i = 3;
+			        	else if(position == 27 || 35 < position && position < 38 || 44 < position && position < 48 || 53 < position && position < 58) i = 1;
+			        	else if(position == 35 || 42 < position && position < 45 || 50 < position && position < 54 || 58 < position && position < 63) i = 2;
 			        	else i=0;
 			        	
 			        	switch (i){ 
@@ -168,5 +177,71 @@ public class ActivityGame extends Activity{
 			return null;
 		}
 	}
+
 	
+	
+	public class CustomView extends View {
+	    private static final float DP_PER_SECONDS = 10;
+
+	    private final float mBallCirfumference;
+	    private final float mBallRadius;
+	    private final Bitmap mBallBitmap;
+	    private final Paint mBallBitmapPaint;
+	    private final Matrix mBallTransformMatrix = new Matrix();
+	    private final float mPxPerSecond;
+
+	    private long mStartTime = -1;
+
+	    public CustomView(Context context) {
+	        super(context);
+	        final Resources res = getResources();
+
+	        // Load the ball bitmap. You probably want to use a better bitmap ;)
+	        mBallBitmap = BitmapFactory.decodeResource(res, R.drawable.sheep);
+
+	        // We need the radius and circumference of the ball for our calculations
+	        // later
+	        mBallRadius = mBallBitmap.getHeight() / 2;
+	        mBallCirfumference = mBallRadius * 2 * (float)Math.PI;
+
+	        // Create ourself a paint object so we can adjust the quality of the
+	        // bitmap drawing
+	        mBallBitmapPaint = new Paint();
+
+	        // Significantly improves quality when drawing transformed bitmaps. Compare
+	        // with when you disable this, which is the default
+	        mBallBitmapPaint.setFilterBitmap(true);
+
+	        // Calculate speed of ball in pixels
+	        mPxPerSecond = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, DP_PER_SECONDS,
+	                res.getDisplayMetrics());
+	    }
+
+	    @Override
+	    protected void onDraw(Canvas canvas) {
+	        super.onDraw(canvas);
+
+	        // Calculate how far into the animation we are
+	        if (mStartTime == -1) {
+	            mStartTime = getDrawingTime();
+	        }
+	        long currentTime = getDrawingTime();
+	        float secondsPassed = (currentTime - mStartTime) / 1000.0f;
+
+	        // Calculate how far the ball has moved and how many degrees it has been
+	        // rotated as a consequence of the movement
+	        float movedDistance = secondsPassed * mPxPerSecond;
+	        float fullRotationsMade = movedDistance / mBallCirfumference;
+	        float rotationInDegrees = fullRotationsMade * 360;
+
+	        // Setup the transformation matrix to simulate a rolling ball
+	        mBallTransformMatrix.reset();
+	        mBallTransformMatrix.postRotate(rotationInDegrees, mBallRadius, mBallRadius);
+	        mBallTransformMatrix.postTranslate(movedDistance, 0);
+	        canvas.drawBitmap(mBallBitmap, mBallTransformMatrix, mBallBitmapPaint);
+
+	        // Force redraw so we get an animation
+	        invalidate();
+	    }
+	}
 }
