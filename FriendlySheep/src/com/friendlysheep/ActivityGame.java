8eac5e3feefa68cfa3cbe43ff762ef8480b12d59
Morten <mortenjansrud@gmail.com>
2014-09-09 18:29:28 +0200

    YOyo

--- FriendlySheep/src/com/friendlysheep/ActivityGame.java
+++ FriendlySheep/src/com/friendlysheep/ActivityGame.java
@@ -8,13 +8,40 @@ import android.widget.Button;
 public class ActivityGame extends Activity{
 
 	//Defining variables
+	private boolean alive;
+	private Button b_left; 
+	private Button b_right;
+	private Button b_top;
+	private Button b_bottom;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_game);
 		
+		setLayout();
 	}
 	
+	private void setLayout(){
+		
+		b_left 	= (Button)findViewById(R.id.b_left);
+		b_right = (Button)findViewById(R.id.b_right);
+		b_top 	= (Button)findViewById(R.id.b_top);
+		b_bottom= (Button)findViewById(R.id.b_bottom);
+		
+	}
+	
+	
+	private void playGame(){
+		
+		while(alive){
+			
+			
+			
+			
+		}
+		
+	}
+
 	
 }
