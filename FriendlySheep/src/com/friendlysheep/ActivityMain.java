e79dbaf142e220102ecd19d80c678eb83e5bd619
Morten <mortenjansrud@gmail.com>
2014-09-09 16:12:19 +0200

    Starting

---
+++ FriendlySheep/src/com/friendlysheep/ActivityMain.java
@@ -0,0 +1,15 @@
+package com.friendlysheep;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class ActivityMain extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
