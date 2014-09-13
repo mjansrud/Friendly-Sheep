d4fea4e2f0dd060ab04077366770e981e6a8efd1
Morten <mortenjansrud@gmail.com>
2014-09-13 21:20:00 +0200

    Yoo

--- FriendlySheep/src/com/friendlysheep/ActivityGame.java
+++ FriendlySheep/src/com/friendlysheep/ActivityGame.java
@@ -1,49 +1,5 @@
 package com.friendlysheep;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.widget.Button;
- 
-
-public class ActivityGame extends Activity{
-
-	//Defining variables
-	private boolean alive;
-	private Button b_left;
-	private Button b_right;
-	private Button b_top;
-	private Button b_bottom;
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_game);
-		
-	}
-	
-	private void setLayout(){
-		
-		
-		
-	}
-	
-	
-	private void playGame(){
-		
-		while(alive){
-			
-			
-			
-			
-		}
-		
-	}
-
-	
-}
-=======
-package com.friendlysheep;
-
 import java.util.ArrayList;
 import java.util.Random;
 import android.annotation.SuppressLint;
@@ -283,6 +239,17 @@ public class ViewDrawPath extends View {
 	        // commit the path to our offscreen
 	        mCanvas.drawPath(mPath,  mPaint);
 	        // kill this so we don't double draw
+	        
+			final Handler h = new Handler();
+			final int delay = 1000; //milliseconds
+
+			h.postDelayed(new Runnable(){
+			    public void run(){
+			        // setAnimation();
+			        h.postDelayed(this, delay);
+			    }
+			}, delay);
+	        
 	        mPath.reset();
         }
 
